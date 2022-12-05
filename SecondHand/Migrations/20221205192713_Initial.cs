d7ae57ee702d88dc19955e859d404770bd17ce7a
luka strumbelj <luka.strumbelj12@gmail.com>
2022-12-05 20:35:25 +0100

    prvic2

---
+++ SecondHand/Migrations/20221205192713_Initial.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecondHand.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "KategorijeCevljis",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImeKategorije = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Spol = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KategorijeCevljis", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "KategorijeOblacilas",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImeKategorije = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Spol = table.Column<string>(type: "nvarchar(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KategorijeOblacilas", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Obutevs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImeCevlja = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SlikaCevljaUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StCevlja = table.Column<double>(type: "float", nullable: false),
+                    opis = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    znamka = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KategorijaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Obutevs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Obutevs_KategorijeCevljis_KategorijaId",
+                        column: x => x.KategorijaId,
+                        principalTable: "KategorijeCevljis",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Oblacilas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImeOblacila = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SlikaOblekeUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    opis = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    KategorijaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Oblacilas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Oblacilas_KategorijeOblacilas_KategorijaId",
+                        column: x => x.KategorijaId,
+                        principalTable: "KategorijeOblacilas",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Oblacilas_KategorijaId",
+                table: "Oblacilas",
+                column: "KategorijaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Obutevs_KategorijaId",
+                table: "Obutevs",
+                column: "KategorijaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Oblacilas");
+
+            migrationBuilder.DropTable(
+                name: "Obutevs");
+
+            migrationBuilder.DropTable(
+                name: "KategorijeOblacilas");
+
+            migrationBuilder.DropTable(
+                name: "KategorijeCevljis");
+        }
+    }
+}
--- SecondHand/Models/KategorijeCevlji.cs
+++ SecondHand/Models/KategorijeCevlji.cs
@@ -4,7 +4,7 @@ namespace SecondHand.Models
 {
     public class KategorijeCevlji
     {
-        public int ID { get; set; };
+        public int ID { get; set; }
         public string ImeKategorije { get; set; }
         public char Spol { get; set; }
         public List<Obutev> Obutve { get; set; }
